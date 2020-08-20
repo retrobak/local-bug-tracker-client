8fb5ffba5f87403ab10e0102bbc2df15954f1d35
Willem <57390156+retrobak@users.noreply.github.com>
2020-08-20 10:57:46 +0200

    Update ClientReporter.php

--- src/ClientReporter.php
+++ src/ClientReporter.php
@@ -69,6 +69,7 @@ class ClientReporter
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(
                 'Content-Type: application/json',
                 'Content-Length: ' . strlen($data_string))
